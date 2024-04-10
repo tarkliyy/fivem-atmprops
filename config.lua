eed68dac0c22513d0a02891ce4cc9fbc5fb8bf0d
Tarkliyy <peachchziza@gmail.com>
2024-04-10 21:49:00 +0700

    feat(client): props add

--- config.lua
+++ config.lua
@@ -1,7 +1,6 @@
 Config = {}
 Config.UseESX = false
 Config.Props = {
-    { name = 'prop_jukebox_02v', coords = { x = 100.0, y = 200.0, z = 300.0, h = 0.0 } },
-    { name = 'prop_jukebox_02v', coords = { x = 150.0, y = 250.0, z = 350.0, h = 90.0 } },
+    { name = 'prop_atm_01', coords = { x = 100.0, y = 200.0, z = 300.0, h = 0.0 } },
     -- เพิ่มจุด spawn prop ได้
 }
